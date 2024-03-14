35b9bb12fe11415704f79d797f3a6c797ef2a9c9
rlanjaiarinaly <rlanjaiarinaly@gmail.com>
2024-03-14 14:54:56 +0100

    created the createURL method

--- cmd/expectTest.go
+++ cmd/expectTest.go
@@ -6,60 +6,17 @@ package cmd
 import (
 	"apitest/core"
 	"errors"
-	"fmt"
 	"io"
 	"net/http"
 	"os"
 	"runtime"
-	"sync"
 
 	"github.com/spf13/cobra"
-	"gopkg.in/yaml.v3"
 )
 
 var (
 	ErrStatusCode = errors.New("Status Code mismatched")
 )
-
-type expectS struct {
-	ExpectS []Expect `yaml:"expects"`
-}
-
-type input struct {
-	Url    string `yaml:"url"`
-	Method string `yaml:"method"`
-}
-
-type expectedOutput struct {
-	StatusCode int `yaml:"statusCode"`
-}
-
-type Expect struct {
-	Input          input          `yaml:"input"`
-	ExpectedOutput expectedOutput `yaml:"expectedOutput"`
-}
-
-type Report struct {
-	core.Result
-	testStatus bool
-	testType   string
-	testErrors []error
-}
-
-func (r *Report) Fprint(out io.Writer) {
-	p := func(format string, args ...any) {
-		fmt.Fprintf(out, format+"\n", args...)
-	}
-	testStatus := "FAILURE"
-	if r.testStatus {
-		testStatus = "SUCCESS"
-	}
-	p("Test [%s]", r.Url)
-	p("\tTest Status: %s", testStatus)
-	p("\tTest Type: %s", r.testType)
-	p("\tTest Errors: %v", r.testErrors)
-	p("\tDuration: %s", r.Duration)
-}
 
 // expectTestCmd represents the expectTest command
 var expectTestCmd = &cobra.Command{
@@ -100,7 +57,7 @@ func testExpectedRouteAction(out io.Writer, filepath string) error {
 	if err != nil {
 		return err
 	}
-	expects, err := readConfig(file)
+	expects, err := core.ReadConfig(file)
 	if err != nil {
 		return err
 	}
@@ -109,57 +66,9 @@ func testExpectedRouteAction(out io.Writer, filepath string) error {
 			MaxIdleConnsPerHost: runtime.NumCPU(),
 		},
 	}
-	for report := range expects.compareOutput(&client) {
+	for report := range expects.CompareOutput(&client) {
 		report.Fprint(out)
 	}
 
 	return nil
-}
-
-func readConfig(file io.Reader) (*expectS, error) {
-	var expect expectS
-	if file != nil {
-		decoder := yaml.NewDecoder(file)
-		if err := decoder.Decode(&expect); err != nil {
-			return nil, err
-		}
-	}
-	return &expect, nil
-}
-
-func (e *Expect) String() string {
-	return fmt.Sprintf("INPUT:\n\turl: %s\n\tmethod: %s\nOUTPUT:\n\tstatusCode: %d", e.Input.Url, e.Input.Method, e.ExpectedOutput.StatusCode)
-}
-
-func (e *expectS) compareOutput(client *http.Client) chan *Report {
-	out := make(chan *Report, len(e.ExpectS))
-	defer close(out)
-
-	wg := sync.WaitGroup{}
-	wg.Add(len(e.ExpectS))
-
-	for _, expect := range e.ExpectS {
-		go func(expect Expect) {
-			defer wg.Done()
-			out <- expect.compareOuput(client)
-		}(expect)
-	}
-	wg.Wait()
-	return out
-}
-
-func (e *Expect) compareOuput(client *http.Client) *Report {
-	req, err := http.NewRequest(e.Input.Method, e.Input.Url, http.NoBody)
-	if err != nil {
-		return &Report{
-			testErrors: []error{err},
-		}
-	}
-	result := core.Send(client, req)
-	report := &Report{testStatus: true, Result: *result}
-	if result.StatusCode != e.ExpectedOutput.StatusCode {
-		report.testStatus = false
-		report.testErrors = append(report.testErrors, fmt.Errorf("%q : got %d expected %d", ErrStatusCode, result.StatusCode, e.ExpectedOutput.StatusCode))
-	}
-	return report
 }
